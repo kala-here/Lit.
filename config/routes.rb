63fe9e23222bbf908b464336e186b4e6346e9403
Catherine Jean <catherinejeanbean@gmail.com>
2017-10-24 22:42:37 -0500

    Add user root paths and Authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   get 'welcome/home'
 
-  root to: 'home#index'
+  get "home", to: "home#index", as: "user_root"
+  
+  root 'home#index'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
